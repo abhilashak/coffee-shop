ec8269438a7e21ea6b5d38e9baec068607a5150a
Abhilash A K <abhilashamballur@gmail.com>
2021-07-11 20:36:59 +0530

    Fix for bootstrap css, Added: header ui, footer ui, font awesome icons, flash msg helper

---
+++ app/controllers/shop_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Handle the shopping!
+class ShopController < ApplicationController
+  # GET /
+  def index
+  end
+end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
+# frozen_string_literal: true
+
+# helper module
 module ApplicationHelper
+  # Fetch bootstrap class for the flash message according to message type
+  def bootstrap_class_for(flash_type)
+    { success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-info' }.stringify_keys[flash_type.to_s] || flash_type.to_s
+  end
+
+  # Creates the flash message content
+  def flash_messages(_opts = {})
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)}", role: 'alert') do
+               concat content_tag(:button, 'x', class: 'close', data: { dismiss: 'alert' })
+               concat message.try(:html_safe)
+             end)
+    end
+    nil
+  end
 end
